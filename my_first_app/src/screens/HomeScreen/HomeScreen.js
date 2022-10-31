18219728951229a9a4204351555077217add2cb8
nicholasbonhoure <nicholasbonhoure@hotmail.com>
2022-10-30 22:22:07 -0400

    NfC Listening Added

--- my_first_app/src/screens/HomeScreen/HomeScreen.js
+++ my_first_app/src/screens/HomeScreen/HomeScreen.js
@@ -1,12 +1,54 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+// import React from 'react'
+// import { View, Text } from 'react-native'
 
-const HomeScreen = () => {
-    return (
-        <View>
-            <Text style={{fontSize: 24, alignSelf: 'center'}}>Home</Text>
-        </View>
-    )
+// const HomeScreen = () => {
+//     return (
+//         <View>
+//             <Text style={{fontSize: 24, alignSelf: 'center'}}>Home</Text>
+//         </View>
+//     )
+// }
+
+// export default HomeScreen;
+
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import NfcManager, {NfcTech} from 'react-native-nfc-manager';
+
+// Pre-step, call this before any NFC operations
+NfcManager.start();
+
+function HomeScreen() {
+  async function readNdef() {
+    try {
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag();
+      console.warn('Tag found', tag);
+    } catch (ex) {
+      console.warn('Oops!', ex);
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest();
+    }
+  }
+
+  return (
+    <View style={styles.wrapper}>
+      <TouchableOpacity onPress={readNdef}>
+        <Text>Scan a Tag</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default HomeScreen;
